7ad46563b37e35c9c5e39d88c07217b77f27f48f
Daiana Carnevali <121942474+DamianCarnevali@users.noreply.github.com>
2023-01-09 16:19:15 -0300

    protfolio terminado

--- js/portafolio.js
+++ js/portafolio.js
@@ -27,5 +27,69 @@
 	} else {
 		logo.src = './img/logo.png';
 	}
+	function servicios_slider() {
+		if ($('.servicio_slider').length) {
+			$('.servicio_slider').owlCarousel({
+				loop: true,
+				margin: 30,
+				items: 2,
+				autoplay: true,
+				smartSpeed: 2500,
+				dots: true,
+				responsiveClass: true,
+				responsive: {
+					0: {
+						items: 1
+					},
+					991: {
+						items: 2
+					}
+				}
+			});
+		}
+	}
+	servicios_slider();
+	$(window).on('load', function () {
+		$('.educacion-filter ul li').on('click', function () {
+			$('.educacion-filter ul li').removeClass('active');
+			$(this).addClass('active');
 
+			var data = $(this).attr('data-filter');
+			$workGrid.isotope({
+				filter: data
+			});
+		});
+
+		if (document.getElementById('educacionf')) {
+			var $workGrid = $('.educaciong').isotope({
+				itemSelector: '.all',
+				percentPosition: true,
+				masonry: {
+					columnWidth: '.all'
+				}
+			});
+		}
+	});
+	function experiencia_slider() {
+		if ($('.experiencia_slider').length) {
+			$('.experiencia_slider').owlCarousel({
+				loop: true,
+				margin: 30,
+				items: 2,
+				autoplay: true,
+				smartSpeed: 2500,
+				dots: true,
+				responsiveClass: true,
+				responsive: {
+					0: {
+						items: 1
+					},
+					991: {
+						items: 2
+					}
+				}
+			});
+		}
+	}
+	experiencia_slider();
 })(jQuery);
